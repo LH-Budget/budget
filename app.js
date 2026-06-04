10760cfea7dbb07c127a652d593c6aa4ac17d380
LH-Budget <larshenneberg@gmail.com>
2026-06-04 22:56:12 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const VERSION='v2.6';
+const VERSION='v2.7';
 const SUPABASE_URL='https://oudjjqvhvgxouoanqvjb.supabase.co';
 const SUPABASE_KEY='sb_publishable_vXbOB_8s8GJVWaJMR5eF8w_R2Dl3WPQ';
 const sb=window.supabase.createClient(SUPABASE_URL,SUPABASE_KEY,{auth:{persistSession:true,autoRefreshToken:true}});
